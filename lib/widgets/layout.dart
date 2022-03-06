aa7196ad9f6e134273e411b3faadedf6cee6261a
Yehuda Kremer <yehudakremer@gmail.com>
2022-03-06 12:29:08 +0200

    fix window border

--- lib/widgets/layout.dart
+++ lib/widgets/layout.dart
@@ -11,7 +11,7 @@ class Layout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(1),
+      padding: const EdgeInsets.all(2),
       child: ClipOval(
         child: ScaffoldPage(
           padding: const EdgeInsets.all(0),
